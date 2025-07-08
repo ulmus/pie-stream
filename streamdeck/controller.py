e3862c433da09dde1716198968655d96cc7cb5c1
Jens Alm <jens.alm@prorenata.se>
2025-07-08 14:12:12 +0200

    Add deck typing

--- streamdeck/controller.py
+++ streamdeck/controller.py
@@ -1,7 +1,7 @@
 import logging
 
-from StreamDeck.DeviceManager import DeviceManager
-from StreamDeck.Devices.StreamDeck import StreamDeck
+from StreamDeck.DeviceManager import DeviceManager  # type: ignore
+from StreamDeck.Devices.StreamDeck import StreamDeck  # type: ignore
 
 logger = logging.getLogger(__name__)
 # StreamDeckController.py
@@ -9,24 +9,25 @@ logger = logging.getLogger(__name__)
 
 class StreamDeckController:
     device_manager: DeviceManager | None = None
-    streamdeck: StreamDeck | None = None
+    deck: StreamDeck
 
     def __init__(self):
         self.device_manager = DeviceManager()
-        self.deck = (
+        deck = (
             self.device_manager.enumerate()[0]
             if self.device_manager.enumerate()
             else None
         )
-        if self.deck is None:
+        if deck is None:
             raise RuntimeError("No Stream Deck device found.")
+        self.deck = deck
         self.key_count = self.deck.key_count()
         self.key_row_length, self.key_column_length = self.deck.key_layout()
         self.deck.open()
         self.deck.reset()
 
         # Set default brightness
-        self.deck.set_brightness(50)
+        self.deck.set_brightness(70)
 
         self.is_connected = True
         logger.info(f"Stream Deck initialized: {self.deck.deck_type()}")
